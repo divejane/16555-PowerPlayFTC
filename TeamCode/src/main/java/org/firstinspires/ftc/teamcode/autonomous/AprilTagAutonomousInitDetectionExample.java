9b77729d9e2f83e79b2b33ceacdbe1a5a044cb23
divejane <105249833+divejane@users.noreply.github.com>
2022-12-10 19:00:47 -0600

    Update AprilTagAutonomousInitDetectionExample.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AprilTagAutonomousInitDetectionExample.java
@@ -57,7 +57,7 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
      - Change the AprilTags (LEFT, MIDDLE, RIGHT) using your choice of AprilTags
      L-> Google "AprilTags PDF" and find the DotProduct3D PDF
      - Change the hardware map using the motor names on your Driver Station
-     - experiment and change the values in the Drive function called near line 205
+     - experiment and change the values in the Drive functions called near line 205
      - Feel free to come to me and ask any questions :)
 
      **/
