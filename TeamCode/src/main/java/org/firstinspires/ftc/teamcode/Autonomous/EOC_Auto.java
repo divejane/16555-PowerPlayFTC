11a477b3b37c4488e10df5eefcaea969ba672cf6
divejane <sonny.nichols5689@gmail.com>
2022-12-12 11:49:32 -0600

    Cleaned up folder names and hierarchy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EOC_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EOC_Auto.java
@@ -19,7 +19,7 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode.autonomous;
+package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
